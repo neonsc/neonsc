39e76c0f12204021b7cbf0b3c346dceedca2e714
neonsc <96820868+neonsc@users.noreply.github.com>
2021-12-29 18:43:40 +0800

    Add files via upload

---
+++ madun.py
@@ -0,0 +1,33 @@
+#!/bin/sh
+# code by madun modsメ
+# script perubah tampilan termux
+
+# tampilan
+tamp1="==============================="
+tamp2=" {      WELCOME TO TERMUX     }
+
+# login termux
+toilet -f big -f gay login
+echo "Masukkan Password" | lolcat
+read pass
+
+# data tampilan
+if [ $pass = hacker ]
+then
+  echo"masukkan title"
+  read title
+ clear
+  figlet $title | lolcat
+  echo $tam1 | lolcat # tampilan 1
+  echo $tam2 | lolcat # tampilan 2
+  echo $tam1 | lolcat # tampilan 1
+  echo
+   echo "# DDOS cyber" | lolcat
+   echo "# Scurity punk" | lolcat
+   echo "# Mafia teknonogi" | lolcat
+     echo $tam1 | lolcat # tampilan 1
+     else
+       echo "Password salah" | lolcat
+        echo $tam1 | lolcat # tampilan 1
+        sh.neon32.sh
+   fi
